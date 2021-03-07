2891e5982fae44f43adf86e92f6f6a2c1f69318d
konstantinos <raftopouloskon@gmail.com>
2021-03-07 15:16:01 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Todo from './Todo';
 import Header from './Header';
 
+let count = 0;
 
 function App() {
   const [input, setInput] = useState(null);
@@ -13,12 +14,40 @@ function App() {
   };
 
   const handleClick = () => {
-    setTodos([...todos, input]);
+    const todo = {
+      text: input,
+      id: count
+    };
+    count++;
+    setTodos([...todos, todo]);
+    setInput('');
   };
-  const handleRemove = (index) => {
-    const temp = todos.filter((t, i) => i !== index);
-    setTodos(temp);
 
+  const handleRemove = (e) => {
+    const temp = todos.filter((t, i) => t.id !== e.id);
+    setTodos(temp);
+  }
+
+  const handleMoveUp = (e) => {
+    const index = todos.indexOf(e);
+    if (index > 0) {
+      const tempArr = todos.filter((t) => t.id !== e.id);
+      tempArr.splice(index - 1, 0, e);
+      console.log(index, 'up');
+
+      setTodos(tempArr);
+    }
+  }
+
+  const handleMoveDown = (e) => {
+    const index = todos.indexOf(e);
+    if (index < todos.length) {
+      const tempArr = todos.filter((t) => t.id !== e.id);
+      tempArr.splice(index + 1, 0, e);
+      console.log(index, 'down');
+
+      setTodos(tempArr);
+    }
   }
 
   return (
@@ -28,7 +57,7 @@ function App() {
       <Header />
       <section className="input" >
         <div>
-          <input className="app__input" placeholder="new Todo.." onChange={getInput} />
+          <input className="app__input" value={input} placeholder="new Todo.." onChange={getInput} />
           <button
             className="app__button"
             onClick={handleClick}
@@ -38,12 +67,26 @@ function App() {
 
       <div className="app__list">
         <div>
-          {todos.map((todo, index) => (
-            <div key={`todo${index}`} className="app__todo">
-              <div >
-                <Todo text={todo} />
+          {todos.map((todo) => (
+            <div key={todo.id} className="app__todo">
+              <div className="app__card" >
+                <Todo text={todo.text} id={todo.id} />
+                <div className="app__card__btns">
+                  <button
+                    className="up"
+                    onClick={() => handleMoveUp(todo)}
+                  > UP
+                    <i className="arrow__up"></i>
+                  </button>
+                  <button
+                    className="down"
+                    onClick={() => handleMoveDown(todo)}
+                  >DOWN
+                    <i className="arrow__down"></i>
+                  </button>
+                </div>
               </div>
-              <button className="app__todo__button" onClick={() => handleRemove(index)} >Remove</button>
+              <button className="app__todo__button" onClick={() => handleRemove(todo)} >Remove</button>
             </div>
           ))}
         </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import './Todo.css'
 
-const Todo = ({ text }) => {
+const Todo = ({ text, id }) => {
     const [done, setDone] = useState(false);
 
+    console.log(id);
     return (
         <div className={`todo${done ? '__done' : ''}`}>
             <input id="checkbox" type="checkbox" onClick={(e) => setDone(e.target.checked)} />
